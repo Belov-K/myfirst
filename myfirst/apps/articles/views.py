1fb775cd305b1998cb4dbe8a879fa17d0a39dd9c
kiril <kaz1nak3@mail.ru>
2023-06-17 21:16:17 +0300

    feat(articles): поменял вывод текста feat(articles): добавил вывод текста feat(articles): добавил статьи и комментарии fix(articles): статьи и комментарии feat(articles): изменил дизайн feat(articles): добавил функцию недавней статьи feat(articles): добавил админа fix(articles): исправил ошибку feat(articles): вывод текста и название вкладки feat(articles): вывел список статей feat(articles): добавил систему комменатриев

--- myfirst/apps/articles/views.py
+++ myfirst/apps/articles/views.py
@@ -1,7 +1,9 @@
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
-from .models import Article, Comment
+from django.urls import reverse
+
+from .models import Article
 
 def index(request):
     latest_articles_list=Article.objects.order_by('-pub_date')[:5]
@@ -13,7 +15,16 @@ def detail(request, article_id):
     except:
         raise Http404("Статься не найдена!")
 
-    return render(request, 'articles/detail.html', {'article': a})
+    latest_comment_list=a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a, 'latest_comment_list': latest_comment_list})
 
 def leave_comment(request, article_id):
-    pass
+    try:
+        a=Article.objects.get(id=article_id)
+    except:
+        raise Http404("Статься не найдена!")
+
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['text'])
+
+    return HttpResponseRedirect( reverse('articles:detail', args=(a.id,)) )
